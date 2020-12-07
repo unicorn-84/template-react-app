77a1bffafe810dac66e627c03e8412c10ac0b086
unicorn-84 <unicorn-84@ya.ru>
2020-12-07 12:23:03 +0300

    First commit

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  test('should render App component', () => {
+    render(<App />);
+    screen.debug();
+  });
+});
