1de62626aec5ab4db01e3a94b7b9a056d38dd4d5
unicorn-84 <unicorn-84@ya.ru>
2021-01-12 18:19:09 +0300

    Upgrade deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () => (<div>App</div>);
+const App = styled.div`
+  font-size: 2rem;
+  color: #333;
+`;
+
+export default () => (<App>App</App>);
